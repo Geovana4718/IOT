ecdf0a1ba3ae44ed86f38ea199e1c6821f3725b7
Geovana4718 <geovana.augusta.sousa@gmail.com>
2024-11-06 11:12:16 -0300

    PROJETO CALCULADORA

---
+++ CALCULADORA/src/main/java/com/mycompany/calculadora/CALCULADORA.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+import VIEWS.TelaPrincipal;
+
+/**
+ *
+ * @author devmat
+ */
+public class CALCULADORA {
+
+    public static void main(String[] args) {
+        //criando objeto da tela da calculadora 
+        TelaPrincipal tela = new TelaPrincipal ();
+        //deixando a tela visivel 
+        tela.setVisible(true);
+    }
+}
